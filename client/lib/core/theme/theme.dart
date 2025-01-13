713583f9d0266fa3aefb7a35a8a6a848f3e6d139
Winn Z <winnwyz@gmail.com>
2025-01-13 13:16:21 +1100

    set theme to dark

---
+++ client/lib/core/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+  );
+}
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
@@ -12,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.darkThemeMode,
       home: const SignupPage(),
     );
   }
